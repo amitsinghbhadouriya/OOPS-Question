38ee923e5276cbc2bb8d35196f16379cc49c52c7
Amit Singh <abhaybhadouriya88@gmail.com>
2025-08-20 23:30:57 +0530

    update question-47

--- Question-47.cpp
+++ Question-47.cpp
@@ -3,33 +3,41 @@
 #include <string>
 using namespace std;
 
-void writeToFile(const string &filename, const string &content) {
+void writeToFile(const string &filename, const string &content)
+{
     ofstream file(filename);
-    if(file.is_open()) {
+    if (file.is_open())
+    {
         file << content;
         file.close();
         cout << "Content written to file successfully." << endl;
     }
-    else {
+    else
+    {
         cout << "Unable to open file for writing." << endl;
     }
 }
 
-void displayFileContent(const string &filename) {
+void displayFileContent(const string &filename)
+{
     ifstream file(filename);
     string line;
-    if(file.is_open()) {
+    if (file.is_open())
+    {
         cout << "Content of" << filename << ":" << endl;
-        while(getline(file,line)){
+        while (getline(file, line))
+        {
             cout << line << endl;
         }
         file.close();
     }
-    else {
+    else
+    {
         cout << "Unable to open file for reading." << endl;
     }
 }
-int main() {
+int main()
+{
     string filename = "test.txt";
     string content = "I love OOPs";
     writeToFile(filename, content);
